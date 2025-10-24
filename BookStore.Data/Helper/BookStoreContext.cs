13bb73eb3cbf02550ee3a8deac535b717842727f
Phat Nguyen <nqphat@tma.com.vn>
2025-10-24 15:37:24 +0700

    Initial Commit

---
+++ BookStore.Data/Helper/BookStoreContext.cs
@@ -0,0 +1,59 @@
+﻿using BookStore.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Data.Helper
+{
+    public partial class BookStoreContext : DbContext
+    {
+        public BookStoreContext() { }
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
+
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                                    .SetBasePath(Directory.GetCurrentDirectory())
+                                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
+                IConfigurationRoot configuration = builder.Build();
+                optionsBuilder.EnableSensitiveDataLogging();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(x => x.Id);
+            modelBuilder.Entity<Category>().Property(x => x.Id);
+            modelBuilder.Entity<User>().Property(x => x.Id);
+            modelBuilder.Entity<Order>().Property(x => x.Id);
+            modelBuilder.Entity<OrderDetail>().HasKey(od => new { od.OrderId, od.BookId });
+
+            modelBuilder.Entity<OrderDetail>()
+                            .HasOne(od => od.Book)
+                            .WithMany(b => b.OrderDetails)
+                            .HasForeignKey(od => od.BookId)
+                            .OnDelete(DeleteBehavior.NoAction);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+}
---
+++ BookStore.Data/Helper/Exception.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Data.Helper
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+
+    public class UnauthorizedException : Exception
+    {
+        public UnauthorizedException(string message) : base(message)
+        {
+        }
+    }
+
+    public class ConflictException : Exception 
+    {
+        public ConflictException(string message) : base(message)
+        {
+        }
+    }
+}
